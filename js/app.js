5f810c9b5713531a61e09cba7d03b830613d4d7e
Eng-Esraa_shamaa <esra2asmaibrahim@gmail.com>
2021-04-20 14:48:26 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var mybutton = document.getElementById("top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+
+function toTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
